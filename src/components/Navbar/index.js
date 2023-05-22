ea897fb2b6ddc7eb2e2b60471a10c48dee654b33
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-05-22 14:47:29 -0300

    Update nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,40 @@
-import styles from './NavBar.modules.scss';
+import styles from './NavBar.module.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import classNames from 'classnames';
+import { RiShoppingCart2Line, RiShoppingCartFill} from 'react-icons/ri';
+
+const iconeProps = {
+  color: 'white',
+  size: 24
+}
 
 function Navbar() {
   return (  
     <nav className={styles.nav}>
       <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a href="/" className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}>
+            Pagina inicial
+          </a>
+        </div>
+      </div>
+      <div className={styles.icones}>
+        <div>
+          <a href='/carrinho'>
+            {window.location.pathname === '/carrinho'
+              ? <RiShoppingCartFill {...iconeProps}/> 
+              : <RiShoppingCart2Line {...iconeProps}/>
+            }
+          </a>
+        </div>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
+
+/* link para o github https://github.com/alura-cursos/trato-tech/tree/Aula1 */

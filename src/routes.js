6496df32b1699cf8dd39c1d53ef684de06629918
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-05-26 08:23:20 -0300

    Create routes.js

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import PaginaPadrao from "components/PaginaPadrao";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
